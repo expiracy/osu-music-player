adf098e62537fed4b07b8f95ca565b9175bd4ad8
James Gray <jameslgray.home@gmail.com>
2023-07-25 14:47:44 +0100

    improved setup

--- setup/DataJsonManager.py
+++ setup/DataJsonManager.py
@@ -3,50 +3,76 @@ import os
 
 
 class DataJsonManager:
-    json_path = f"{os.getcwd()}\\osu_music_player_data.json"
 
     def __init__(self):
+        json_path = DataJsonManager.get_data_json_path()
         try:
             # Load the data file if it exists
-            with open(self.json_path, 'r') as file:
+            with open(json_path, 'r') as file:
                 data = json.load(file)
 
-        except Exception:
+        except (IOError, ValueError):
             # Resets the file if there were any issues
-            with open(self.json_path, 'w') as file:
+            with open(json_path, 'w') as file:
                 data = {"osu_path": "", "favourites": []}
                 json.dump(data, file)
 
         self.osu_path = data["osu_path"]
         self.favourites = data["favourites"]
 
-        if not self.validate_osu_path(self.osu_path):
-            self.osu_path = self.ask_for_osu_path()
+        if not DataJsonManager.validate_osu_path(self.osu_path):
+            self.osu_path = self.osu_path_setup()
             self.save_data("osu_path", self.osu_path)
 
-    def validate_osu_path(self, osu_path):
+    def osu_path_setup(self):
+        print("[osu! Path Configuration]")
+        print("1. Default osu! folder path location")
+        print("2. Manual osu! folder path input")
+        choice = input(">")
+
+        if choice == '1':
+            osu_path = DataJsonManager.get_default_osu_path()
+        elif choice == '2':
+            print()
+            print("[Manual Configuration]")
+            print("Enter exact file path to osu folder")
+            print("(e.g. C:/Users/{YOUR-USERNAME}/AppData/Local/osu!)")
+            osu_path = input(">").strip("/").strip("\\")
+        else:
+            return self.osu_path_setup()
+
+        if not DataJsonManager.validate_osu_path(osu_path):
+            return self.osu_path_setup()
+
+        print()
+        print("[osu! Path Set]")
+        print(osu_path)
+        print()
+
+        return osu_path
+
+    @staticmethod
+    def validate_osu_path(osu_path):
         try:
             return "Songs" in os.listdir(osu_path)
         except IOError:
             return False
 
-    def ask_for_osu_path(self):
-        osu_path = input(
-            "Enter exact file path to osu folder "
-            "(e.g. C:/Users/{YOUR-USERNAME}/AppData/Local/osu!)"
-            "\n>"
-        ).strip("/").strip("\\")
+    @staticmethod
+    def get_data_json_path():
+        return f"{os.getcwd()}\\osu_music_player_data.json"
 
-        if not self.validate_osu_path(osu_path):
-            return self.ask_for_osu_path()
-
-        return osu_path
+    @staticmethod
+    def get_default_osu_path():
+        return f"{os.getenv('LOCALAPPDATA')}\\osu!"
 
     @staticmethod
     def save_data(key, value):
-        with open(DataJsonManager.json_path, 'r') as file:
+        json_path = DataJsonManager.get_data_json_path()
+
+        with open(json_path, 'r') as file:
             data = json.load(file)
             data[key] = value
 
-        with open(DataJsonManager.json_path, 'w') as file:
+        with open(json_path, 'w') as file:
             json.dump(data, file)
